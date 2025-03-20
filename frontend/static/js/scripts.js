0540163c982fb3039151e207eb2e0047ae536435
emmabeni27 <larisabeni@mail.com>
2025-03-20 12:29:07 -0300

    ojito mágico

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -190,3 +190,21 @@ document.getElementById('registerAdminForm').addEventListener('submit', function
         backToLoginFromAdminBtn.click();
     }, 2000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".toggle-password").forEach(button => {
+        button.addEventListener("click", function () {
+            let targetId = this.getAttribute("data-target");
+            let passwordInput = document.getElementById(targetId);
+            let icon = this.querySelector("i");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                icon.classList.replace("bi-eye", "bi-eye-slash");
+            } else {
+                passwordInput.type = "password";
+                icon.classList.replace("bi-eye-slash", "bi-eye");
+            }
+        });
+    });
+});
