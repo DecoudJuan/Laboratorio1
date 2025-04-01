b6aa1190d58ff1e0a20c0581fe3327025f53fe04
DecoudJuan <juandecoud05@hotmail.com>
2025-04-01 12:37:47 -0300

    creo la API

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:qwerty@localhost:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+config = {
+    'development': DevelopmentConfig,
+}
