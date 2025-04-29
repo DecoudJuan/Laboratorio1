001093d3661dd98343b06f107f572d18f9494160
emmabeni27 <larisabeni@mail.com>
2025-04-28 22:33:28 -0300

    importar js

--- frontend/static/js/admin.js
+++ frontend/static/js/admin.js
@@ -160,3 +160,12 @@ function deleteUser(username) {
         });
     }
 }
+
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Eliminar token y datos de usuario del localStorage
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('currentUser');
+    
+    // Redireccionar a la página de inicio de sesión
+    window.location.href = 'index.html';
+});
